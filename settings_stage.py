fa788921bc6e4fa6e67b7aad392adef6e50a5c4e
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2011-08-10 15:45:33 -0400

    add stage settings/conf

---
+++ settings_stage.py
@@ -0,0 +1,12 @@
+from settings import *
+
+DATABASE_ENGINE = 'postgresql_psycopg2'
+
+TEMPLATE_DIRS = (
+    "/usr/local/share/sandboxes/common/mvsim/mvsim/templates",
+)
+
+MEDIA_ROOT = '/usr/local/share/sandboxes/common/mvsim/uploads/'
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
