436e9ec8275cfe364a853a56d915d5ce63f45e06
Anders Pearson <anders@columbia.edu>
2012-05-24 10:40:51 -0400

    add statsd/graphite

--- settings_shared.py
+++ settings_shared.py
@@ -36,6 +36,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     )
 
 MIDDLEWARE_CLASSES = (
+    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
+    'django_statsd.middleware.GraphiteMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -79,7 +81,14 @@ INSTALLED_APPS = (
     'registration',
     'main',
     'south',
+    'django_statsd',
 )
+
+STATSD_CLIENT = 'statsd.client'
+STATSD_PREFIX = 'mvsim'
+STATSD_HOST = 'localhost'
+STATSD_PORT = 8125
+STATSD_PATCHES = ['django_statsd.patches.db', ]
 
 ACCOUNT_ACTIVATION_DAYS = 7
 
