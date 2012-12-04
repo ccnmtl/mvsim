ab37c61669818070a8dd7f0b077a76a5bfd5cbe2
Anders Pearson <anders@columbia.edu>
2012-12-04 11:39:21 -0500

    flake8

--- graph/tests.py
+++ graph/tests.py
@@ -1,11 +1,5 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -20,4 +14,3 @@ Another way to test that 1 + 1 is equal to 2.
 >>> 1 + 1 == 2
 True
 """}
-
