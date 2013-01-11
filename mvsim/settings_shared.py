cfcd3f4d274bff08b3c01e19f3fb1d90fddf4926
Anders Pearson <anders@columbia.edu>
2013-01-11 15:32:20 -0500

    add django-debug-toolbar

--- mvsim/settings_shared.py
+++ mvsim/settings_shared.py
@@ -47,6 +47,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'django.middleware.transaction.TransactionMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'courseaffils.middleware.CourseManagerMiddleware',
     'djangohelpers.middleware.AuthRequirementMiddleware',
 )
@@ -84,6 +85,20 @@ INSTALLED_APPS = (
     'south',
     'django_statsd',
     'smoketest',
+    'debug_toolbar',
+)
+
+INTERNAL_IPS = ('127.0.0.1', )
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
 )
 
 STATSD_CLIENT = 'statsd.client'
