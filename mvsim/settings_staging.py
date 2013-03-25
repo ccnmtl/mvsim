7b9b62f33700a97bfb0ba1dcf65e17886be693db
Anders Pearson <anders@columbia.edu>
2013-03-25 10:28:21 -0400

    staging config

---
+++ mvsim/settings_staging.py
@@ -0,0 +1,49 @@
+# flake8: noqa
+from settings_shared import *
+
+TEMPLATE_DIRS = (
+    "/var/www/mvsim/mvsim/mvsim/templates",
+)
+
+MEDIA_ROOT = '/var/www/mvsim/uploads/'
+# put any static media here to override app served static media
+STATICMEDIA_MOUNTS = (
+    ('/sitemedia', '/var/www/mvsim/mvsim/sitemedia'),
+)
+
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mvsim',
+        'HOST': '',
+        'PORT': 6432,
+        'USER': '',
+        'PASSWORD': '',
+        }
+}
+STATSD_PREFIX = 'mvsim-staging'
+
+if 'migrate' not in sys.argv:
+    SENTRY_SITE = 'mvsim-stage'
+    SENTRY_SERVERS = ['http://sentry.ccnmtl.columbia.edu/sentry/store/']
+
+    import logging
+    from raven.contrib.django.handlers import SentryHandler
+    logger = logging.getLogger()
+    # ensure we havent already registered the handler
+    if SentryHandler not in map(type, logger.handlers):
+        logger.addHandler(SentryHandler())
+        logger = logging.getLogger('sentry.errors')
+        logger.propagate = False
+        logger.addHandler(logging.StreamHandler())
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
+MVSIM_GRAPH_OUTPUT_DIRECTORY = "/var/www/mvsim/uploads/graphs"
