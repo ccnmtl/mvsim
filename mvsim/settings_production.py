d96b1fd710c233ab4cdf0d81ba6d624a5fa7e784
Nikolas Nyby <nikolas@gnu.org>
2019-11-08 13:11:35 -0500

    migrate to sentry-sdk

--- mvsim/settings_production.py
+++ mvsim/settings_production.py
@@ -1,7 +1,9 @@
-# flake8: noqa
-from .settings_shared import *
+from django.conf import settings
+from mvsim.settings_shared import *  # noqa: F403
 from ccnmtlsettings.production import common
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 project = 'mvsim'
 base = os.path.dirname(__file__)
@@ -10,14 +12,20 @@ locals().update(
     common(
         project=project,
         base=base,
-        STATIC_ROOT=STATIC_ROOT,
-        INSTALLED_APPS=INSTALLED_APPS,
+        STATIC_ROOT=STATIC_ROOT,  # noqa: F405
+        INSTALLED_APPS=INSTALLED_APPS,  # noqa: F405
         cloudfront='d3l9lx77lf1ppr',
     ))
 
 try:
-    from mvsim.local_settings import *
+    from mvsim.local_settings import *  # noqa: F403
 except ImportError:
     pass
 
 MVSIM_GRAPH_OUTPUT_DIRECTORY = "/var/www/mvsim/uploads/graphs"
+
+if hasattr(settings, 'SENTRY_DSN'):
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,  # noqa: F405
+        integrations=[DjangoIntegration()],
+    )
